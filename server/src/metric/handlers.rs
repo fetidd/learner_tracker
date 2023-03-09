dcd1b66df4d6027d15c37c7f975680e07bec491b
Ben Jones <fetiddius@gmail.com>
2023-03-09 12:54:06 +0000

    new way to layoput handlers

--- server/src/metric/handlers.rs
+++ server/src/metric/handlers.rs
@@ -6,3 +6,20 @@ use axum::{
 };
 use serde_json::json;
 use uuid::Uuid;
+
+use super::model::Metric;
+
+pub async fn create_metric(
+    State(app_state): State<AppState>,
+    Json(metric_data): Json<Metric>,
+) -> Result<StatusCode> {
+    Ok(handle_create_metric(app_state, metric_data).await?)
+}
+
+async fn handle_create_metric(app_state: AppState, metric_data: Metric) -> Result<StatusCode> {
+    Ok(StatusCode::CREATED)
+}
+
+
+
+
